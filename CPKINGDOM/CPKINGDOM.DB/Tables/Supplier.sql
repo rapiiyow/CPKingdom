9f2afefcad1648db96954333371d88d3790dea3f
John Rafael Tan <johnrafaeltan94@gmail.com>
2022-10-16 12:32:56 +0800

    Added Supplier Table

---
+++ CPKINGDOM/CPKINGDOM.DB/Tables/Supplier.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Supplier]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(50) NULL, 
+    [Address] NVARCHAR(250) NULL, 
+    [ContactPerson] NVARCHAR(250) NULL, 
+    [ContactNo] NVARCHAR(50) NULL
+)
